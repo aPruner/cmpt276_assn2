1a980a35cae2cd5d2f1619e4236856e82f02cc98
Adam Pruner <adampruner99@gmail.com>
2016-10-14 13:50:38 -0700

    add methods to models

--- app/models/tokimon.rb
+++ app/models/tokimon.rb
@@ -1,3 +1,19 @@
 class Tokimon < ApplicationRecord
   belongs_to :trainer
+
+  validates(:name, presence: true)
+  validates(:height, presence: true)
+  validates(:weight, presence: true)
+  validates(:fly, presence: true)
+  validates(:fight, presence: true)
+  validates(:fire, presence: true)
+  validates(:water, presence: true)
+  validates(:electric, presence: true)
+  validates(:ice, presence: true)
+  validates(:total, presence: true)
+  validates(:trainer_id, presence: true)
+
+  def self.set_total(toki)
+    toki.total = toki.fly + toki.fight + toki.fire + toki.water + toki.electric + toki.ice
+  end
 end
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,3 +1,15 @@
 class Trainer < ApplicationRecord
   has_many :tokimons
+
+  validates(:name, presence: true)
+  validates(:level, presence: true)
+  validates(:num_tokimon, presence: true)
+
+  def self.set_level(train)
+    train.level = train.num_tokimon / 3
+  end
+
+  def self.add_tokimon(train)
+  	train.num_tokimon += 1
+  end
 end
